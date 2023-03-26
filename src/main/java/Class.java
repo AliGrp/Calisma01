0db62be84e597ff4ed057f121ff67bbe25de8ec5
AliGrp <122046419+aly120372906@users.noreply.github.com>
2023-03-26 21:07:29 +0300

    v3

--- src/main/java/Class.java
+++ src/main/java/Class.java
@@ -2,5 +2,6 @@ public class Class {
     public static void main(String[] args) {
         System.out.println("MERHABA");
         System.out.println("selam");
+        System.out.println("slm");
     }
 }
