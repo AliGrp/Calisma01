7cc83b6fa382a2ec70f6bb19aebf1f7738cc0222
isminizsibel <email_adresinizsibelinko33@gmail.com>
2023-03-26 21:28:31 +0300

    v7

--- src/main/java/Class.java
+++ src/main/java/Class.java
@@ -4,6 +4,13 @@ public class Class {
         System.out.println("selam");
         System.out.println("slm");
         System.out.println("sibel");
+
+
+
+
+
+
+
         System.out.println("sibel2");
     }
 }
